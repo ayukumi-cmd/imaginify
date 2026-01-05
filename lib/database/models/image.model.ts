14b27d558c6a4d245baa31f1a10605dbdb100c5a
ayukumi-cmd <98808592+ayukumi-cmd@users.noreply.github.com>
2026-01-05 18:27:03 +0000

    change

--- lib/database/models/image.model.ts
+++ lib/database/models/image.model.ts
@@ -1,6 +1,7 @@
-import { Document, Schema, model, models } from "mongoose";
+import { Document, Schema, model, models, Types } from "mongoose";
 
 export interface IImage extends Document {
+  _id: Types.ObjectId;
   title: string;
   transformationType: string;
   publicId: string;
